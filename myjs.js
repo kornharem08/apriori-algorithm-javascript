670dd871d051dbf8641f5faf64c317797f6a2a48
tanakorn <tana_korn01@hotmail.com>
2019-03-08 20:46:36 +0700

    backen+fonten99.99

--- myjs.js
+++ myjs.js
@@ -35,6 +35,8 @@ function apriori(itemsetinput) {
   
   var dataset2 = [];
   var frequent = [];
+  var html=''
+  html += '<label class="mb-2 mr-sm-2 ">Large Itemsets (by Apriori):</label> <br>'
   for (var key in uniqs) {
     var itemset = (uniqs[key] / transaction) * 100;
     if (itemset >= minsup) {
@@ -45,6 +47,8 @@ function apriori(itemsetinput) {
         
       frequent.push({ key: [key], sup: itemset })
       console.log("{"+key+"}"+" ("+itemset +")");
+      html+='{'+key+'}'+' ( Support: '+itemset +')<br>';
+      document.getElementById('subSetItem').innerHTML = html; 
     }
     
   }
@@ -61,6 +65,7 @@ function apriori(itemsetinput) {
 
 
   var subset = getAllSubsets(dataset2)
+var html=''
 
   subset.forEach(function (element) {
     //console.log(element);
@@ -73,9 +78,12 @@ function apriori(itemsetinput) {
           count += 1
         }
       })
+      
       if ((count / transaction) * 100 >= minsup) {
         console.log("{"+element+"}"+" ( Support: "+(count / transaction) * 100 +"% )");
         frequent.push({ key: element, sup: (count / transaction) * 100 })
+        html+='{'+element+'}'+' ( Support: '+(count / transaction) * 100 +'% )<br>'
+        document.getElementById('subSet').innerHTML = html; 
       }
       
       
@@ -96,6 +104,8 @@ function apriori(itemsetinput) {
 
 
   var ip = 0
+  var html=''
+  html += '<label class="mb-2 mr-sm-2 ">Association Rules</label> <br>'
   frequent.forEach(function (assosition) {
     var subset2 = getAllSubsets(assosition.key)
     // console.log(subset2);
@@ -110,6 +120,8 @@ function apriori(itemsetinput) {
             var remove = removeFromArray(assosition.key, assosition2)
             console.log("{"+assosition2.toString()+"}", '==>', "{"+remove.toString()+"}",'( Support :',assosition.sup +"% )" ,', ( Confidence :', (assosition.sup / assosition3.sup) * 100,"% )");
             ip += 1
+            html+='{'+assosition2.toString()+'} ==>{'+remove.toString()+'} ( Support :'+assosition.sup +'% ) , ( Confidence :'+ (assosition.sup / assosition3.sup) * 100+'% )<br>'
+            document.getElementById('setItem').innerHTML = html;
           }
           }
           
@@ -120,6 +132,9 @@ function apriori(itemsetinput) {
     });
   });
   console.log(ip);
+
+
+
 
   function removeFromArray(original, remove) {
     return original.filter(value => !remove.includes(value));
@@ -132,15 +147,17 @@ function table() {
   var table = document.getElementById("table").value;
   tid = table;
   var html = "";
-
+html += '<label class="mb-2 mr-sm-2 ">Enter Items</label>';
 for (var i = 0; i < table; i++) {
     html += '<input type="text"class="form-control" name="' + i + '" id="' + i + '" aria-describedby="helpId" placeholder=""></input>'+'<br>';   
   }
    console.log(html)
     html +=
-    '<div id="button1" style="text-align: center"><button type="button"  onclick="inputarr()">OK</button> </div>';
+
+    // '<div><button id="button1" type="button" class="btn btn-outline-danger" onclick="inputarr()>Apriori</button></div>';
+    // '<div id="button1" style="text-align: center"><button type="button"  onclick="inputarr()">OK</button> </div>';
+    // '<div > <button type="button"  onclick="inputarr()  class="btn btn-danger" >Apriori</button></div>';
    document.getElementById('showbox').innerHTML = html;
-  
 }
 
 function inputarr() {
