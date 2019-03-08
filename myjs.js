ef2a303e989fce61439788dc12216ecc1820de48
tanakorn <tana_korn01@hotmail.com>
2019-03-08 23:51:27 +0700

    FINISH

--- myjs.js
+++ myjs.js
@@ -6,14 +6,14 @@ function apriori(itemsetinput) {
   var result = [];
   var temp;
 
-  
+
   for (var i = 0; i < dataset.length; i++) {
     temp = dataset[i].length;
     for (var j = 0; j < temp; j++) {
       result.push(dataset[i][j])
     }
   }
- 
+
 
 
   // console.log(getAllSubsets(result));
@@ -28,44 +28,47 @@ function apriori(itemsetinput) {
   var confident = document.getElementById("ConfidentSupport").value;
   console.log(confident);
   console.log(transaction);
-  
-  
+
+
   var minsup = document.getElementById("MinimumSupport").value;
   console.log(minsup);
-  
+
   var dataset2 = [];
   var frequent = [];
-  var html=''
-  html += '<label class="mb-2 mr-sm-2 ">Large Itemsets (by Apriori):</label> <br>'
+  var numcount = 0;
+  var headli = ''
+  var countheadli = '';
+
   for (var key in uniqs) {
     var itemset = (uniqs[key] / transaction) * 100;
     if (itemset >= minsup) {
+      numcount++
       //console.log(key+":"+itemset);
       dataset2.push(key);
-      
-        // console.log({ key: [key], sup: itemset });
-        
-      frequent.push({ key: [key], sup: itemset })
-      console.log("{"+key+"}"+" ("+itemset +")");
-      html+='{'+key+'}'+' ( Support: '+itemset +')<br>';
-      document.getElementById('subSetItem').innerHTML = html; 
-    }
-    
-  }
-  
- 
 
-    const getAllSubsets =
-      theArray => theArray.reduce(
-        (subsets, value) => subsets.concat(
-          subsets.map(set => [value, ...set])
-        ),
-        [[]]
-      );
+      // console.log({ key: [key], sup: itemset });
+
+      frequent.push({ key: [key], sup: itemset })
+      console.log("{" + key + "}" + " (" + itemset + ")");
+      headli += '{' + key + '}' + ' ( Support: ' + itemset + ')<br>';
+
+    }
+
+  }
+
+
+
+  const getAllSubsets =
+    theArray => theArray.reduce(
+      (subsets, value) => subsets.concat(
+        subsets.map(set => [value, ...set])
+      ),
+      [[]]
+    );
 
 
   var subset = getAllSubsets(dataset2)
-var html=''
+  var html = ''
 
   subset.forEach(function (element) {
     //console.log(element);
@@ -78,20 +81,22 @@ var html=''
           count += 1
         }
       })
-      
+
       if ((count / transaction) * 100 >= minsup) {
-        console.log("{"+element+"}"+" ( Support: "+(count / transaction) * 100 +"% )");
+        numcount++
+        console.log("{" + element + "}" + " ( Support: " + (count / transaction) * 100 + "% )");
         frequent.push({ key: element, sup: (count / transaction) * 100 })
-        html+='{'+element+'}'+' ( Support: '+(count / transaction) * 100 +'% )<br>'
-        document.getElementById('subSet').innerHTML = html; 
+        html += '{' + element + '}' + ' ( Support: ' + (count / transaction) * 100 + '% )<br>'
+
       }
-      
-      
     }
   });
 
+  countheadli += '<label class="mb-2 mr-sm-2 ">' + numcount + ' Large Itemsets (by Apriori):</label> <br>'
+  document.getElementById('showcountlg').innerHTML = countheadli;
+  document.getElementById('subSetItem').innerHTML = headli;
+  document.getElementById('subSet').innerHTML = html;
 
-  // console.log(JSON.stringify(frequent));
 
 
   function contains(haystack, needles) {
@@ -101,39 +106,31 @@ var html=''
   }
 
 
+  var num = 0
+  var html = ''
+  var headasso = ''
 
-
-  var ip = 0
-  var html=''
-  html += '<label class="mb-2 mr-sm-2 ">Association Rules</label> <br>'
   frequent.forEach(function (assosition) {
     var subset2 = getAllSubsets(assosition.key)
-    // console.log(subset2);
     subset2.forEach(function (assosition2) {
-     // console.log(assosition2.length);
-      
       if (assosition2 != '' && assosition2.length != assosition.key.length) {
-          
         frequent.forEach(function (assosition3) {
-          if((assosition.sup / assosition3.sup) * 100 >= confident){
+          if ((assosition.sup / assosition3.sup) * 100 >= confident) {
             if (assosition2.sort().toString() == assosition3.key.sort().toString()) {
-            var remove = removeFromArray(assosition.key, assosition2)
-            console.log("{"+assosition2.toString()+"}", '==>', "{"+remove.toString()+"}",'( Support :',assosition.sup +"% )" ,', ( Confidence :', (assosition.sup / assosition3.sup) * 100,"% )");
-            ip += 1
-            html+='{'+assosition2.toString()+'} ==>{'+remove.toString()+'} ( Support :'+assosition.sup +'% ) , ( Confidence :'+ (assosition.sup / assosition3.sup) * 100+'% )<br>'
-            document.getElementById('setItem').innerHTML = html;
+              var remove = removeFromArray(assosition.key, assosition2)
+              console.log("{" + assosition2.toString() + "}", '==>', "{" + remove.toString() + "}", '( Support :', assosition.sup + "% )", ', ( Confidence :', (assosition.sup / assosition3.sup) * 100, "% )");
+              num += 1
+              html += '{' + assosition2.toString() + '} ==>{' + remove.toString() + '} ( Support :' + assosition.sup + '% ) , ( Confidence :' + (assosition.sup / assosition3.sup) * 100 + '% )<br>'
+            }
           }
-          }
-          
         });
-
       }
-
     });
   });
-  console.log(ip);
-
-
+  headasso += '<label class="mb-2 mr-sm-2 ">' + num + ' Association Rules</label> <br>'
+  document.getElementById('headaso').innerHTML = headasso;
+  document.getElementById('setItem').innerHTML = html;
+  console.log(num);
 
 
   function removeFromArray(original, remove) {
@@ -147,18 +144,27 @@ function table() {
   var table = document.getElementById("table").value;
   tid = table;
   var html = "";
-html += '<label class="mb-2 mr-sm-2 ">Enter Items</label>';
-for (var i = 0; i < table; i++) {
-    html += '<input type="text"class="form-control" name="' + i + '" id="' + i + '" aria-describedby="helpId" placeholder=""></input>'+'<br>';   
-  }
-   console.log(html)
-    html +=
 
-    // '<div><button id="button1" type="button" class="btn btn-outline-danger" onclick="inputarr()>Apriori</button></div>';
-    // '<div id="button1" style="text-align: center"><button type="button"  onclick="inputarr()">OK</button> </div>';
-    // '<div > <button type="button"  onclick="inputarr()  class="btn btn-danger" >Apriori</button></div>';
-   document.getElementById('showbox').innerHTML = html;
+ 
+  
+  html += '<label class="mb-2 mr-sm-2 ">Enter Items</label>';
+  for (var i = 0; i < table; i++) {
+
+    html += '<input type="text"class="form-control" name="' + i + '" id="' + i + '" aria-describedby="helpId" placeholder=""></input>' + '<br>';
+
+  }
+  console.log(html)
+  if(table !=0){
+  html +=
+
+    '<div><center><button id="button1" type="button" class="btn btn-outline-danger" onclick="inputarr()">Apriori</button></center></div>';
+    
+    document.getElementById('showbox').innerHTML = html;
+  }else{
+    document.getElementById("showbox").innerHTML = "";
+  }
 }
+
 
 function inputarr() {
   var itemsetinput = []
